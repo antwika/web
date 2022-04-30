27abcc41265828323735aeb3bce272bbc9aa1338
Anton Wiklund <ixuz07@gmail.com>
2022-04-30 22:41:06 +0200

    fix: remove unused import

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import ExpandingBox from '../components/ExpandingBox'
 import Logotype from '../components/Logotype'
 import TypedText from '../components/TypedText'
