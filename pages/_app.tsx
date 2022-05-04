4765ae1833f99ebc450b89c5d332b25c49921578
Anton Wiklund <ixuz07@gmail.com>
2022-05-04 18:26:36 +0200

    chore: wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { IntlProvider } from 'react-intl';
-import { LOCALES } from "../i18n/locales";
-import { MESSAGES } from "../i18n/messages";
-
-const locale = 'en-US';
-// const locale = 'sv-SE';
-// const locale = 'ko-KR';
 
 function _app({ Component, pageProps }: AppProps) {
   return <>
-    <IntlProvider
-      messages={MESSAGES[locale]}
-      locale={locale}
-      defaultLocale={LOCALES.ENGLISH}>
-      <Component {...pageProps} />
-    </IntlProvider>
+    <Component {...pageProps} />
   </>
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from 'next'
 import HomePage from './HomePage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { IntlProvider } from 'react-intl';
+import { LOCALES } from "../i18n/locales";
+import { MESSAGES } from "../i18n/messages";
+
+const locale = 'en-US';
+// const locale = 'sv-SE';
+// const locale = 'ko-KR';
 
 const RootPage: NextPage = () => {
-  return <HomePage />;
+  return (
+    <IntlProvider
+      messages={MESSAGES[locale]}
+      locale={locale}
+      defaultLocale={LOCALES.ENGLISH}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </IntlProvider>
+  );
 }
 
 export default RootPage;
