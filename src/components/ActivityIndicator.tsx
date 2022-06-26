b9b245a3183e7aaa3d04f93cdea544a923d0521a
Anton Wiklund <ixuz07@gmail.com>
2022-06-26 19:42:08 +0200

    fix: styling mistakes

--- src/components/ActivityIndicator.tsx
+++ src/components/ActivityIndicator.tsx
@@ -5,7 +5,7 @@ import styles from './ActivityIndicator.module.css';
 const ActivityIndicator = () => {
   const { theme } = useContext(ThemeContext);
   return (
-    <div data-testid='activity-indicator' className={styles.activityIndicator} style={{ backgroundColor: theme.neutral[300] }}></div>
+    <div data-testid='activity-indicator' className={styles.activityIndicator} style={{ backgroundColor: theme.neutral[300].bg }}></div>
   );
 }
 
--- src/components/ActivityIndicatorOverlay.tsx
+++ src/components/ActivityIndicatorOverlay.tsx
@@ -6,7 +6,7 @@ import styles from './ActivityIndicatorOverlay.module.css';
 const ActivityIndicatorOverlay = () => {
   const { theme } = useContext(ThemeContext);
   return (
-  <div data-testid='activity-indicator-overlay' className={styles.container} style={{ backgroundColor: theme.neutral[300] }}>
+  <div data-testid='activity-indicator-overlay' className={styles.container} style={{ backgroundColor: theme.neutral[300].bg }}>
     <ActivityIndicator />
   </div>
   );
